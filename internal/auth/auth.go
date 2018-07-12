de24be952425d3936bb9b0d0dd6f642663f0e698
Aleksandr Razumov <ar@cydev.ru>
2018-07-13 00:38:43 +0300

    auth: remove unneeded unsafe code

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -4,7 +4,6 @@ package auth
 import (
 	"errors"
 	"sync"
-	"unsafe"
 
 	"github.com/gortc/stun"
 )
@@ -29,15 +28,6 @@ type Response struct {
 	Integrity stun.MessageIntegrity
 }
 
-// b2s converts byte slice to a string without memory allocation.
-// See https://groups.google.com/forum/#!msg/Golang-Nuts/ENgbUzYvCuU/90yGx7GUAgAJ .
-//
-// Note it may break if string and/or slice header will change
-// in the future go versions.
-func b2s(b []byte) string {
-	return *(*string)(unsafe.Pointer(&b))
-}
-
 func (s *Static) Auth(m *stun.Message) (stun.MessageIntegrity, error) {
 	var (
 		username stun.Username
@@ -48,7 +38,7 @@ func (s *Static) Auth(m *stun.Message) (stun.MessageIntegrity, error) {
 		return nil, err
 	}
 	s.mux.RLock()
-	i := s.credentials[b2s(username)]
+	i := s.credentials[string(username)]
 	s.mux.RUnlock()
 	if i == nil {
 		return nil, errors.New("user not found")
