2949b4952f70250f541aca90e7ed0a44708b16c4
Aleksandr Razumov <ar@cydev.ru>
2018-07-15 23:09:20 +0300

    server: move WriteHeader call after transaction id set in context

--- internal/server/server.go
+++ internal/server/server.go
@@ -156,10 +156,11 @@ func (c *context) build(class stun.MessageClass, method stun.Method, s ...stun.S
 		return nil
 	}
 	c.response.Reset()
+	// Setting response type and transaction ID.
 	c.response.Type.Class = class
 	c.response.Type.Method = method
-	c.response.WriteHeader()
 	copy(c.response.TransactionID[:], c.request.TransactionID[:])
+	c.response.WriteHeader()
 	if err := c.apply(&c.nonce, &c.realm); err != nil {
 		return err
 	}
