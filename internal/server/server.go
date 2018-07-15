b65893228ac60f48bf90073e70a37b7d569a2178
Aleksandr Razumov <ar@cydev.ru>
2018-07-16 01:51:55 +0300

    server: update comment to Server

--- internal/server/server.go
+++ internal/server/server.go
@@ -15,9 +15,8 @@ import (
 
 // Server is RFC 5389 basic server implementation.
 //
-// Current implementation is UDP only and not utilizes FINGERPRINT mechanism,
-// nor ALTERNATE-SERVER, nor credentials mechanisms. It does not support
-// backwards compatibility with RFC 3489.
+// Current implementation is UDP only and not ALTERNATE-SERVER.
+// It does not support backwards compatibility with RFC 3489.
 type Server struct {
 	log    *zap.Logger
 	allocs *allocator.Allocator
