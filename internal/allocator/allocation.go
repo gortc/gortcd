af01b306faa3f835bc5e6de19b2233e8fa478388
Aleksandr Razumov <ar@cydev.ru>
2018-08-12 17:33:03 +0300

    Revert "allocator: use turn.{Addr,FiveTuple} and add aliases"
    This reverts commit 78ee183

--- internal/allocator/allocation.go
+++ internal/allocator/allocation.go
@@ -13,12 +13,55 @@ import (
 )
 
 // Addr is ip:port.
-// DEPRECATED: use turn.Addr.
-type Addr = turn.Addr
+type Addr struct {
+	IP   net.IP
+	Port int
+}
+
+// FromUDPAddr sets addr to UDPAddr.
+func (a *Addr) FromUDPAddr(n *net.UDPAddr) {
+	a.IP = n.IP
+	a.Port = n.Port
+}
+
+// Equal returns true if b == a.
+func (a Addr) Equal(b Addr) bool {
+	if a.Port != b.Port {
+		return false
+	}
+	return a.IP.Equal(b.IP)
+}
+
+func (a Addr) String() string {
+	return fmt.Sprintf("%s:%d", a.IP, a.Port)
+}
 
 // FiveTuple represents 5-TUPLE value.
-// DEPRECATED: use turn.FiveTuple.
-type FiveTuple = turn.FiveTuple
+type FiveTuple struct {
+	Client Addr
+	Server Addr
+	Proto  turn.Protocol
+}
+
+func (t FiveTuple) String() string {
+	return fmt.Sprintf("%s->%s (%s)",
+		t.Client, t.Server, t.Proto,
+	)
+}
+
+// Equal returns true if b == t.
+func (t FiveTuple) Equal(b FiveTuple) bool {
+	if t.Proto != b.Proto {
+		return false
+	}
+	if !t.Client.Equal(b.Client) {
+		return false
+	}
+	if !t.Server.Equal(b.Server) {
+		return false
+	}
+	return true
+}
 
 // PeerHandler represents handler for data that is sent to relayed address
 // of allocation.
