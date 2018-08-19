f9107a72af4f9643756cae170698b8a4a2da17a1
Aleksandr Razumov <ar@cydev.ru>
2018-08-19 21:39:07 +0300

    e2e:webrtc: capture packets

--- e2e/webrtc-chrome/test.sh
+++ e2e/webrtc-chrome/test.sh
@@ -16,13 +16,31 @@ printf "${GREEN}Go version \"${CI_GO_VERSION}\"${NC}\n"
 
 # kill and remove any running containers
 cleanup () {
+  docker stop ci_gortcd_tcpdump
+  docker rm -f ci_gortcd_tcpdump
   docker-compose -p ci kill
-  docker-compose -p ci rm -f 
+  docker-compose -p ci rm -f
+  docker network rm gortcd_e2e_webrtc
 }
 
 # catch unexpected failures, do cleanup and output an error message
 trap 'cleanup ; printf "${RED}Tests Failed For Unexpected Reasons${NC}\n"'\
   HUP INT QUIT PIPE TERM
+
+# PREPARING NETWORK CAPTURE
+docker network create gortcd_e2e_webrtc --internal
+docker build -t gortc/tcpdump -f tcpdump.Dockerfile .
+
+NETWORK_ID=`docker network inspect gortcd_e2e_webrtc -f "{{.Id}}"`
+NETWORK_SUBNET=`docker network inspect gortcd_e2e_webrtc -f "{{(index .IPAM.Config 0).Subnet}}"`
+CAPTURE_INTERFACE="br-${NETWORK_ID:0:12}"
+
+echo "will capture traffic on $CAPTURE_INTERFACE$"
+
+docker run -e INTERFACE=${CAPTURE_INTERFACE} -e SUBNET=${NETWORK_SUBNET} -d \
+    -v $(pwd):/root/dump \
+    --name ci_gortcd_tcpdump --net=host gortc/tcpdump
+
 
 # build and run the composed services
 docker-compose -p ci build && docker-compose -p ci up -d
@@ -38,6 +56,7 @@ TEST_EXIT_CODE=`docker wait ci_turn-controlling_1`
 docker logs ci_turn-controlling_1 &> log-controlling.txt
 docker logs ci_turn-controlled_1 &> log-controlled.txt
 docker logs ci_turn-server_1 &> log-server.txt
+docker logs ci_gortcd_tcpdump &> log-tcpdump.txt
 
 cat log-controlling.txt
 
@@ -55,4 +74,3 @@ cleanup
 
 # exit the script with the same code as the test service code
 exit ${TEST_EXIT_CODE}
-
