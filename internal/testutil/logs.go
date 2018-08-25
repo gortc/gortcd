3ffae68368c178b44e806134321c539326f47f81
Aleksandr Razumov <ar@cydev.ru>
2018-08-25 10:47:12 +0300

    testutil:log: use t.Helper()

--- internal/testutil/logs.go
+++ internal/testutil/logs.go
@@ -9,6 +9,7 @@ import (
 
 // EnsureNoErrors calls the t.Error if there are any ErrorLevel entries in logs.
 func EnsureNoErrors(t *testing.T, logs *observer.ObservedLogs) {
+	t.Helper()
 	for _, e := range logs.TakeAll() {
 		if e.Level == zapcore.ErrorLevel {
 			t.Error(e.Message)
