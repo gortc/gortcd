342ce695e33da10e872e3ec4e1f4608a7fd8274a
Aleksandr Razumov <ar@gortc.io>
2019-05-05 07:06:14 +0300

    cli: refactor reload command and add test for it

--- internal/cli/reload.go
+++ internal/cli/reload.go
@@ -8,57 +8,65 @@ import (
 	"strings"
 
 	"github.com/spf13/cobra"
+	"github.com/spf13/pflag"
 	"github.com/spf13/viper"
+	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
 )
+
+func execReload(v *viper.Viper, f *pflag.FlagSet, stdout io.Writer) {
+	logCfg, logErr := getZapConfig(v)
+	if logErr != nil {
+		panic(logErr)
+	}
+	silent, err := f.GetBool("silent")
+	if err != nil {
+		panic(err)
+	}
+	if silent {
+		// Override level to silent logs.
+		logCfg.Level.SetLevel(zapcore.WarnLevel)
+	}
+	log, buildErr := logCfg.Build()
+	if buildErr != nil {
+		panic(buildErr)
+	}
+	l := log.Sugar()
+	if cfgPath := v.ConfigFileUsed(); len(cfgPath) > 0 {
+		l.Infow("config file used", "path", v.ConfigFileUsed())
+	} else {
+		l.Info("default configuration used")
+	}
+	if strings.Split(v.GetString("version"), ".")[0] != "1" {
+		l.Fatalw("unsupported config file version", "v", v.GetString("version"))
+	}
+	apiAddr := v.GetString("api.addr")
+	if apiAddr == "" {
+		l.Fatal("no api.addr config set")
+	}
+	u := "http://" + apiAddr + "/reload"
+	res, httpErr := http.Get(u) // #nosec
+	if httpErr != nil {
+		l.Fatalw("failed to perform http request", "err", httpErr)
+	}
+	if res.StatusCode != http.StatusOK {
+		l.Fatalw("unexpected status code", "code", res.StatusCode, "status", res.Status)
+	}
+	body := new(bytes.Buffer)
+	if _, err = io.Copy(body, res.Body); err != nil {
+		l.Warn("failed to read body", "err", err)
+	}
+	if _, err = fmt.Fprintln(stdout, "OK", "-", strings.TrimSpace(body.String())); err != nil {
+		l.Warn("write to stdout failed", zap.Error(err))
+	}
+}
 
 var reloadCmd = &cobra.Command{
 	Use:   "reload",
 	Short: "notify server about config change via api",
 	Run: func(cmd *cobra.Command, args []string) {
 		v := viper.GetViper()
-		logCfg, logErr := getZapConfig(v)
-		if logErr != nil {
-			panic(logErr)
-		}
-		silent, err := cmd.Flags().GetBool("silent")
-		if err != nil {
-			panic(err)
-		}
-		if silent {
-			// Override level to silent logs.
-			logCfg.Level.SetLevel(zapcore.WarnLevel)
-		}
-		log, buildErr := logCfg.Build()
-		if buildErr != nil {
-			panic(buildErr)
-		}
-		l := log.Sugar()
-		if cfgPath := v.ConfigFileUsed(); len(cfgPath) > 0 {
-			l.Infow("config file used", "path", v.ConfigFileUsed())
-		} else {
-			l.Info("default configuration used")
-		}
-		if strings.Split(v.GetString("version"), ".")[0] != "1" {
-			l.Fatalw("unsupported config file version", "v", v.GetString("version"))
-		}
-		apiAddr := v.GetString("api.addr")
-		if apiAddr == "" {
-			l.Fatal("no api.addr config set")
-		}
-		u := "http://" + apiAddr + "/reload"
-		res, httpErr := http.Get(u) // #nosec
-		if httpErr != nil {
-			l.Fatalw("failed to perform http request", "err", httpErr)
-		}
-		if res.StatusCode != http.StatusOK {
-			l.Fatalw("unexpected status code", "code", res.StatusCode, "status", res.Status)
-		}
-		body := new(bytes.Buffer)
-		if _, err = io.Copy(body, res.Body); err != nil {
-			l.Warn("failed to read body", "err", err)
-		}
-		fmt.Println("OK", "-", strings.TrimSpace(body.String()))
+		execReload(v, cmd.Flags(), cmd.OutOrStdout())
 	},
 }
 
---
+++ internal/cli/reload_test.go
@@ -0,0 +1,34 @@
+package cli
+
+import (
+	"bytes"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+)
+
+func TestReload(t *testing.T) {
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		switch r.URL.Path {
+		case "/reload":
+			if _, err := fmt.Fprintln(w, "Reloaded"); err != nil {
+				t.Error(err)
+			}
+		default:
+			w.WriteHeader(http.StatusNotFound)
+		}
+	}))
+	defer server.Close()
+
+	v := getViper()
+	v.Set("api.addr", server.Listener.Addr())
+	flags := reloadCmd.Flags()
+	_ = flags.Set("silent", "false")
+	buf := new(bytes.Buffer)
+	execReload(v, flags, buf)
+	if s := buf.String(); strings.TrimSpace(s) != "OK - Reloaded" {
+		t.Errorf("unexpected output: %s", s)
+	}
+}
